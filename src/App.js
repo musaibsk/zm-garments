cc18583f703a376eda7ef86e343e3eb1c1812044
Musaib <musaib_shaikh@hotmail.com>
2020-05-05 00:08:37 +0530

    React router dom implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const Hats = () => (
+  <h1>
+    Hello
+  </h1>
+)
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hatspage' component={Hats} />
+      </Switch>
     </div>
   );
 }
